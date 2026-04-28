2c34d6a6ec1719fe3c9bc9e764a7081c5a36375b
ghbm-itk <123645708+ghbm-itk@users.noreply.github.com>
2026-04-28 12:18:05 +0200

    Update process.py

--- robot_framework/process.py
+++ robot_framework/process.py
@@ -1,6 +1,5 @@
 """This module contains the main process of the robot."""
 
-import os
 from datetime import date
 
 from OpenOrchestrator.orchestrator_connection.connection import OrchestratorConnection
